6f5962a1aa232deccf467f31146c73a9a7f7a39b
bg4zz <b-inozemtsev@list.ru>
2025-06-09 06:29:28 +0500

    rework all structure + animations in btns, anchors, dropdowns

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,6 +1,38 @@
 // выпадающий список
 const dropdowns = document.querySelectorAll('.custom-dropdown');
 
+// dropdowns.forEach(dropdown => {
+//     const toggle = dropdown.querySelector('.custom-dropdown-toggle');
+//     const menu = dropdown.querySelector('.custom-dropdown-menu');
+
+//     dropdown.addEventListener('mouseenter', () => {
+//         menu.classList.add('active');
+//     });
+
+//     dropdown.addEventListener('mouseleave', () => {
+//         menu.classList.remove('active');
+//     });
+
+//     dropdown.addEventListener('focus', () => {
+//         menu.classList.add('active');
+//     });
+
+//     dropdown.addEventListener('blur', () => {
+//         menu.classList.remove('active');
+//     });
+
+//     document.addEventListener('click', function (event) {
+//         if (!dropdown.contains(event.target)) {
+//             menu.classList.remove('active');
+//         }
+//     });
+
+//     toggle.addEventListener('click', (e) => {
+//         e.preventDefault();
+//         menu.classList.toggle('active');
+//     });
+// });
+
 dropdowns.forEach(dropdown => {
     const toggle = dropdown.querySelector('.custom-dropdown-toggle');
     const menu = dropdown.querySelector('.custom-dropdown-menu');
@@ -10,6 +42,14 @@ dropdowns.forEach(dropdown => {
     });
 
     dropdown.addEventListener('mouseleave', () => {
+        menu.classList.remove('active');
+    });
+
+    toggle.addEventListener('focus', () => {
+        menu.classList.add('active');
+    });
+
+    toggle.addEventListener('blur', () => {
         menu.classList.remove('active');
     });
 
